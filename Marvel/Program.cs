6996efa19d3c3da1d77ca17e6f0b4dc69924e74b
dataGriff <griff182uk@yahoo.co.uk>
2024-01-01 12:36:21 +0000

    stuff

--- Marvel/Program.cs
+++ Marvel/Program.cs
@@ -1,31 +1,33 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+// using System.Net.Http;
 using Marvel.Client;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Kiota.Http.HttpClientLibrary;
 
-    string publicApiKey = "";
-    string privateApiKey = "";
+string publicApiKey = "";
+string privateApiKey = "";
 
 
 // API requires no authentication, so use the anonymous
 // authentication provider
-var authProvider = new CustomAuthProvider(publicApiKey,privateApiKey);
+//var authProvider = new CustomAuthProvider(publicApiKey, privateApiKey);
+var authProvider = new AnonymousAuthenticationProvider();
 // Create request adapter using the HttpClient-based implementation
 var adapter = new HttpClientRequestAdapter(authProvider);
 // Create the API client
 var client = new MarvelClient(adapter);
 
-  
-        using var md5 = MD5.Create();
-        string timestamp = DateTime.Now.Ticks.ToString();
-        var data = Encoding.UTF8.GetBytes(timestamp + privateApiKey + publicApiKey);
-        var hashedKey = md5.ComputeHash(data);
-        var hashedKeyString = BitConverter.ToString(hashedKey).Replace("-", "").ToLower();
 
-        var notclient = new HttpClient();
-        var request = new HttpRequestMessage(HttpMethod.Get, $"https://gateway.marvel.com:443/v1/public/characters?apikey={publicApiKey}&hash={hashedKeyString}&ts={timestamp}&nameStartsWith=spider&limit=10");
-        
+using var md5 = MD5.Create();
+string timestamp = DateTime.Now.Ticks.ToString();
+var data = Encoding.UTF8.GetBytes(timestamp + privateApiKey + publicApiKey);
+var hashedKey = md5.ComputeHash(data);
+var hashedKeyString = BitConverter.ToString(hashedKey).Replace("-", "").ToLower();
+
+var notclient = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, $"https://gateway.marvel.com:443/v1/public/characters?apikey={publicApiKey}&hash={hashedKeyString}&ts={timestamp}&nameStartsWith=spider&limit=10");
+//client.Characters.RequestAdapter.BaseUrl = $"https://gateway.marvel.com:443/v1/public/characters?apikey={publicApiKey}&hash={hashedKeyString}&ts={timestamp}&nameStartsWith=spider&limit=10";
 
 try
 {
@@ -40,9 +42,11 @@ try
 
     Console.WriteLine("Finished not using client.");
 
-        // GET /posts
-    // var allCharacters = await client.Characters.GetAsync();
-    // Console.WriteLine($"{allCharacters}");
+   
+
+    // GET /posts
+    var allCharacters = await client.Characters.GetAsync();
+    Console.WriteLine($"{allCharacters}");
 
 
     // // GET /posts/{id}
@@ -57,31 +61,53 @@ catch (Exception ex)
     Console.WriteLine(ex.StackTrace);
 }
 
-public class CustomAuthProvider : IAuthenticationProvider
-{
-    private readonly string _publicApiKey;
-    private readonly string _privateApiKey;
 
-    public CustomAuthProvider(string publicApiKey, string privateApiKey)
-    {
-        _publicApiKey = publicApiKey;
-        _privateApiKey = privateApiKey;
-    }
+// public class CustomAuthProvider : IAuthenticationProvider
+// {
+//     private readonly string _publicApiKey;
+//     private readonly string _privateApiKey;
 
-    public async Task AuthenticateRequestAsync(HttpRequestMessage request)
-    {
-        using var md5 = MD5.Create();
-        string timestamp = DateTime.Now.Ticks.ToString();
-        var data = Encoding.UTF8.GetBytes(timestamp + privateApiKey + publicApiKey);
-        var hashedKey = md5.ComputeHash(data);
-        var hashedKeyString = BitConverter.ToString(hashedKey).Replace("-", "").ToLower();
+//     public CustomAuthProvider(string publicApiKey, string privateApiKey)
+//     {
+//         _publicApiKey = publicApiKey;
+//         _privateApiKey = privateApiKey;
+//     }
 
-        var uriBuilder = new UriBuilder(request.Uri);
-        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
-        query["apikey"] = _publicApiKey;
-        query["hash"] = hashedKeyString;
-        query["timestamp"] = timestamp;
-        uriBuilder.Query = query.ToString();
-        request.Uri = uriBuilder.Uri;
-    }
-}
+//     public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+//     {
+//         // Implement your authentication logic here
+//         // Example for a hypothetical API that uses MD5 hash of keys and timestamp
+
+//         // Create a timestamp for this request
+//         var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+
+//         // Concatenate the timestamp, private key, and public key
+//         var toBeHashed = timestamp + _privateApiKey + _publicApiKey;
+
+//         // Create the MD5 hash
+//         string hash;
+//         using (var md5 = MD5.Create())
+//         {
+//             var hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(toBeHashed));
+//             hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+//         }
+
+//         // Append the authentication query parameters
+//         var query = HttpUtility.ParseQueryString(request.RequestUri.Query);
+//         query["apikey"] = _publicApiKey;
+//         query["hash"] = hash;
+//         query["ts"] = timestamp;
+
+//         var uriBuilder = new UriBuilder(request.RequestUri)
+//         {
+//             Query = query.ToString()
+//         };
+
+//         request.RequestUri = uriBuilder.Uri;
+
+//         // If there are additional headers or bearer tokens required, set them here
+//         // request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "your_access_token");
+
+//         // Note: No need to return anything as you're directly modifying the HttpRequestMessage
+//     }
+// }
